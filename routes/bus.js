c28eccbee5a61257466cfeefa9873d4a0c73c17d
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2022-12-14 00:12:17 +0700

    update #7

--- routes/bus.js
+++ routes/bus.js
@@ -4,10 +4,4 @@ const router = express.Router();
 const getBus = require("../services/bus/get-bus");
 router.get("/", getBus);
 
-const getJurusan = require("../services/bus/filter-bus");
-router.get("/filter", getJurusan);
-
-const postJadwal = require("../services/bus/post-bus");
-router.post("/", postJadwal);
-
 module.exports = router;
